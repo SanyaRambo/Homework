b1f7d36b390fae7b8cd866a10de504e718800899
Sanya <wasd305@mail.ru>
2025-12-04 13:53:29 +0300

    Optimized editorconfig 2.0

--- vite(React)-hw/src/App.jsx
+++ vite(React)-hw/src/App.jsx
@@ -8,29 +8,33 @@ function App() {
 
 	return (
 		<>
-		<div>
-			<a href="https://vite.dev" target="_blank">
-			<img src={viteLogo} className="logo" alt="Vite logo" />
-			</a>
-			<a href="https://react.dev" target="_blank">
-			<img src={reactLogo} className="logo react" alt="React logo" />
-			</a>
-		</div>
-		<h1>Vite + React</h1>
-		<div className="card">
-			<button onClick={() => setCount((count) => count + 1)}>
-			Нажми на счётчик {count}
-			</button>
-			<p>
-			Редактируй <code>src/App.jsx</code> и сохрани, чтобы протестировать
-			HMR
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="React logo"
+					/>
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					Нажми на счётчик {count}
+				</button>
+				<p>
+					Редактируй <code>src/App.jsx</code> и сохрани, чтобы
+					протестировать HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
 			</p>
-		</div>
-		<p className="read-the-docs">
-			Click on the Vite and React logos to learn more
-		</p>
 		</>
 	);
-	}
+}
 
-	export default App;
+export default App;
