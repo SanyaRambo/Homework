b9a08e73527045954fe649c852eceba93a65aadc
Sanya <wasd305@mail.ru>
2025-12-06 16:32:22 +0300

    code on Declaration style. Is adding new component

--- vite-react-hw/src/App.jsx
+++ vite-react-hw/src/App.jsx
@@ -1,79 +1,53 @@
 // Декларативный код
-import { useState, createElement, Fragment} from "react";
+import { useState, Fragment } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { MyComponent } from "./NewComponent";
 // Императивный код
 function App() {
 	const [count, setCount] = useState(0);
-	const year = new Date().getFullYear()
-// Декларативный код
+	const year = new Date().getFullYear();
+	// Декларативный код
 	return (
-		createElement(
-			Fragment,
-			null,
-			createElement(
-				'div',
-				null,
-                createElement(
-				'a',
-				{href: "https://vite.dev",target: "_blank"},
-					createElement(
-						'img',
-						{ src: viteLogo, className: "logo", alt: "Vite logo" }
-					)
-				),
-				createElement(
-					'a',
-					{href: "https://react.dev",target: "_blank"},
-					createElement(
-						'img',
-						{ src: reactLogo, className: "logo react", alt: "React logo" }
-					)
-				),
-		),
-		createElement('h1', null, "Vite + React"),
-		createElement(
-			'div',
-			{ className: "card"},
-			createElement(
-				'button',
-				{ onClick: () => setCount((count) => count + 1) },
-				`Нажми на счётчик ${count}`
-			),
-			createElement(
-				'p',
-				null,
-				'Редактируй ', createElement('code', null, 'src/App.jsx'),
-				' чтобы протестировать HMR'
-			)
-		),
-		createElement(
-			'p',
-			null,
-			createElement(
-				'span',
-				{ className: "read-the-docs" },
-				"Кликни на Vite или React, чтобы узнать больше"
-			)
-		),
-		createElement(
-			'span',
-			{ className: "read-the-docs"},
-			"Время"
-		),
-		createElement(
-			'br',
-		),
-		createElement(
-			'span',
-			{ className: "read-the-docs"},
-			`${year}`
-		)
-		)
+		<Fragment>
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="React logo"
+					/>
+				</a>
+			</div>
+
+			<h1>Vite + React</h1>
+
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					Нажми на счётчик {count}
+				</button>
+				<p>
+					Редактируй <code>src/App.jsx</code> чтобы протестировать HMR
+				</p>
+			</div>
+
+			<p>
+				<span className="read-the-docs">
+					Кликни на Vite или React, чтобы узнать больше
+				</span>
+			</p>
+
+			<span className="read-the-docs">Время</span>
+			<br />
+			<span className="read-the-docs">{year}</span>
+
+			<MyComponent type="normal" value="123" />
+		</Fragment>
 	);
 }
 
 export default App;
-
-
