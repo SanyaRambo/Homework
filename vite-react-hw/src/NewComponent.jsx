c77aacfa8dc2038b630f280d89631d5e912cb03c
Sanya <wasd305@mail.ru>
2025-12-08 17:43:39 +0300

    is adding list - ul, li

--- vite-react-hw/src/NewComponent.jsx
+++ vite-react-hw/src/NewComponent.jsx
@@ -1,22 +1,19 @@
 import { useState } from "react"
 
-const getTimeFromDate = (date) => String(date).substring(15,24);
+const products = [
+	{id: 'wasd', name: 'Клавиатура'},
+	{id: 'dldo', name: 'Дилдо'}
+]
 
 export const MyComponent = (props) => {
 
-	const [currentDate, setCurrentDate] = useState(new Date());
-
-	setTimeout(() => {
-		setCurrentDate(new Date());
-		console.log(currentTime)
-	}, 1000)
-
-
-	const currentTime = getTimeFromDate(currentDate)
 
 	return (
 		<>
-			<div className="read-the-docs" >{currentTime}</div>
+			<ul>
+				{products.map(({id, name}) => (<li key={id}>{ name }</li>))}
+			</ul>
+
 		</>
 	)
 };
