cb217082ac57da7afe67b11812d4c21478bfb207
Sanya <wasd305@mail.ru>
2025-12-11 13:43:32 +0300

    adding new style

--- vite-react-hw/src/App.jsx
+++ vite-react-hw/src/App.jsx
@@ -13,12 +13,12 @@ function App() {
 		<Fragment>
 			<div>
 				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className={styles.logoVite} alt="Vite logo" />
+					<img src={viteLogo} className={styles.logo} alt="Vite logo" />
 				</a>
 				<a href="https://react.dev" target="_blank">
 					<img
 						src={reactLogo}
-						className={styles.logoReact}
+						className={styles.logo.react}
 						alt="React logo"
 					/>
 				</a>
